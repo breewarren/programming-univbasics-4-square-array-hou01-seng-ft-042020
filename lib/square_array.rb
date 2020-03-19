54601ace20d3ebf6fed623f3b10022e8e3d9ab6d
Bree Warren <ambreea.warren@gmail.com>
2020-03-19 03:21:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  numbers = [1, 2, 3]
+  counter = 0
+  while numbers[counter] do
+    puts number[counter]
+    counter += 1
+  end
 end
+
+0.upto(10) {|x|
+  p [x, Math.sqrt(x), Math.sqrt(x)**2]
+}
+
